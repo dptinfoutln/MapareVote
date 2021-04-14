6a1aca152445009ee2b0ca0502938fc31e1b00a1
El-Sashok <reaubourg.alex@gmail.com>
2021-04-14 17:52:05 +0200

    JWT auth (in develop)

---
+++ Server/src/main/java/fr/univtln/mapare/security/MySecurityContext.java
@@ -0,0 +1,47 @@
+package fr.univtln.mapare.security;
+
+import fr.univtln.mapare.security.filter.request.BasicAuthenticationFilter;
+import fr.univtln.mapare.security.filter.request.JsonWebTokenFilter;
+import jakarta.ws.rs.core.SecurityContext;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.security.Principal;
+
+/**
+ * This class define a specific security context after an authentication with either the basic or the JWT filters.
+ *
+ * @see BasicAuthenticationFilter
+ * @see JsonWebTokenFilter
+ */
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor(staticName = "newInstance")
+public class MySecurityContext implements SecurityContext {
+    private final String authenticationScheme;
+    private final String username;
+
+    //the authenticated user
+    @Override
+    public Principal getUserPrincipal() {
+        return InMemoryLoginModule.USER_DATABASE.getUser(username);
+    }
+
+    //A method to check if a user belongs to a role
+    @Override
+    public boolean isUserInRole(String role) {
+        return InMemoryLoginModule.USER_DATABASE.getUserRoles(username).contains(InMemoryLoginModule.Role.valueOf(role));
+    }
+
+    //Say the access has been secured
+    @Override
+    public boolean isSecure() {
+        return true;
+    }
+
+    //The authentication scheme (Basic, JWT, ...) that has been used.
+    @Override
+    public String getAuthenticationScheme() {
+        return authenticationScheme;
+    }
+}
---
+++ Server/src/main/java/fr/univtln/mapare/security/annotations/JWTAuth.java
@@ -0,0 +1,21 @@
+package fr.univtln.mapare.security.annotations;
+
+import fr.univtln.mapare.security.filter.request.JsonWebTokenFilter;
+import jakarta.ws.rs.NameBinding;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * A annotation for method to be secured with Java Web Token (JWT)
+ * @see JsonWebTokenFilter
+ */
+@NameBinding
+@Retention(RUNTIME)
+@Target({TYPE, METHOD})
+public @interface JWTAuth {
+}
