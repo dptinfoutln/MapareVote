25be25e0184b0f5ee36add8520a1797bd24c59f3
Nico <nico.marguerit@gmail.com>
2021-04-15 19:28:21 +0200

    adds some test needed for my understanding (flush, commit, etc ...

--- Server/src/test/java/fr/univtln/mapare/MainTest.java
+++ Server/src/test/java/fr/univtln/mapare/MainTest.java
@@ -1,12 +1,173 @@
 package fr.univtln.mapare;
 
 import static org.junit.Assert.assertTrue;
+
+import fr.univtln.mapare.model.Choice;
+import fr.univtln.mapare.model.User;
+import fr.univtln.mapare.model.Vote;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+
 public class MainTest {
-    @Test
-    public void shouldAnswerWithTrue()
+    public static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("maparevotedb");
+
+    User personneTest = new User("nico@nico.mail",
+            "MARGUERIT",
+            "Nicolas",
+            "AZERTY",
+            true,
+            false,
+            false);
+
+    Vote voteTest = new Vote("Est-ce que les tests sont toujours utiles ?",
+            LocalDate.now(),
+            null,
+            "algoDeTest",
+            false,
+            false,
+            personneTest);
+
+    Choice choiceTest = new Choice(Arrays.asList("oui_test", "non_test", "blanc_test"),
+            voteTest);
+
+    private User getUserTest()
     {
-        assertTrue( true );
+        EntityManager entityManager = EMF.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        User userTest = (User) entityManager.createNamedQuery("User.findByEmail").setParameter("email", personneTest.getEmail()).getResultList().get(0);
+
+        entityManager.flush();
+        transaction.commit();
+        entityManager.close();
+
+        return userTest;
+    }
+
+    private Vote getVoteTest()
+    {
+        EntityManager entityManager = EMF.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Vote voteTest = (Vote) entityManager.createNamedQuery("Vote.findByVotemaker").setParameter("votemaker", getUserTest()).getResultList().get(0);
+
+        entityManager.flush();
+        transaction.commit();
+        entityManager.close();
+
+        return voteTest;
+    }
+
+    private Choice getChoiceTest()
+    {
+        EntityManager entityManager = EMF.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Choice choiceTest = (Choice) entityManager.createNamedQuery("Choice.findByVote").setParameter("vote", getVoteTest()).getResultList().get(0);
+
+        entityManager.flush();
+        transaction.commit();
+        entityManager.close();
+
+        return choiceTest;
+    }
+
+    @Test
+    public void persistUser()
+    {
+        EntityManager entityManager = EMF.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        entityManager.persist(personneTest);
+
+        entityManager.flush();
+        transaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    public void persistVote()
+    {
+        EntityManager entityManager = EMF.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        User personne = getUserTest();
+        voteTest.setVotemaker(personne);
+
+        entityManager.persist(voteTest);
+
+        entityManager.flush();
+        transaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    public void persistChoice()
+    {
+        EntityManager entityManager = EMF.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Vote vote = getVoteTest();
+        choiceTest.setVote(vote);
+
+        entityManager.persist(choiceTest);
+
+        entityManager.flush();
+        transaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    public void removeChoice()
+    {
+        EntityManager entityManager = EMF.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        entityManager.remove(getChoiceTest());
+
+        entityManager.flush();
+        transaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    public void removeVote()
+    {
+        EntityManager entityManager = EMF.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        entityManager.remove(getVoteTest());
+
+        entityManager.flush();
+        transaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    public void removeUser()
+    {
+        EntityManager entityManager = EMF.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        entityManager.remove(getUserTest());
+
+        entityManager.flush();
+        transaction.commit();
+        entityManager.close();
     }
 }
