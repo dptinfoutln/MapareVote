0e3713e1f2f4fab1c31c2d3297341090fea7780b
El-Sashok <reaubourg.alex@gmail.com>
2021-04-19 18:45:20 +0200

    change api url

--- Web/src/environments/environment.ts
+++ Web/src/environments/environment.ts
@@ -2,12 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import {HttpHeaders} from "@angular/common/http";
+import {HttpHeaders} from '@angular/common/http';
 
 export const environment = {
   production: false,
-  // apiURL: 'https://api.maparevote.siannos.fr/',
-  apiURL: 'http://localhost:5431/',
+  apiURL: 'https://api.maparevote.siannos.fr/',
+  // apiURL: 'http://localhost:5431/',
   headers: new HttpHeaders({'Content-Type': 'application/json; charset=utf-8'})
 };
 
