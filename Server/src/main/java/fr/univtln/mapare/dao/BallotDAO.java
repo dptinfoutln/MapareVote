b775f85195e95697f403218433dd84ad7c01b467
François Palma <francois-palma@etud.univ-tln.fr>
2021-04-19 10:47:34 +0200

    Merging

--- Server/src/main/java/fr/univtln/mapare/dao/BallotDAO.java
+++ Server/src/main/java/fr/univtln/mapare/dao/BallotDAO.java
@@ -14,33 +14,33 @@ import java.util.List;
 public class BallotDAO {
     public static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("maparevotedb");
 
-    public static void persist(List<Choice> choices, User voter, Vote vote, LocalDateTime time) {
-        EntityManager entityManager = EMF.createEntityManager();
-        EntityTransaction transaction = entityManager.getTransaction();
-        transaction.begin();
-
-        // Persist Ballot
-        Ballot B = new Ballot(time, vote);
-        B.setVoter(voter); // TODO: change for anonymous
-        entityManager.persist(B);
-        entityManager.flush();
-
-        for ( Choice C : choices) {
-            // Persist Choice
-            entityManager.persist(C);
-            entityManager.flush();
-            // Persist BallotChoice
-            BallotChoice BC = new BallotChoice(B, C);
-            entityManager.persist(BC);
-            entityManager.flush();
-            // Updates Ballot
-            B.addChoice(BC);
-            entityManager.persist(B);
-            entityManager.flush();
-        }
-        transaction.commit();
-        entityManager.close();
-    }
+//    public static void persist(List<Choice> choices, User voter, Vote vote, LocalDateTime time) {
+//        EntityManager entityManager = EMF.createEntityManager();
+//        EntityTransaction transaction = entityManager.getTransaction();
+//        transaction.begin();
+//
+//        // Persist Ballot
+//        Ballot B = new Ballot(time, vote);
+//        B.setVoter(voter); // TODO: change for anonymous
+//        entityManager.persist(B);
+//        entityManager.flush();
+//
+//        for ( Choice C : choices) {
+//            // Persist Choice
+//            entityManager.persist(C);
+//            entityManager.flush();
+//            // Persist BallotChoice
+//            BallotChoice BC = new BallotChoice(B, C);
+//            entityManager.persist(BC);
+//            entityManager.flush();
+//            // Updates Ballot
+//            B.addChoice(BC);
+//            entityManager.persist(B);
+//            entityManager.flush();
+//        }
+//        transaction.commit();
+//        entityManager.close();
+//    }
 
     public static void persist(Ballot ballot, int voteId, int userId) {
 //        Vote vote = Controllers.Votes.mapGet(voteId);
