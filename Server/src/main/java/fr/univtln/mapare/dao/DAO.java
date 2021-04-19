eae3f46a379b56dbde5cb3d104b358d26dae6345
Nico <nico.marguerit@gmail.com>
2021-04-19 21:14:16 +0200

    adds transaction into DAO

--- Server/src/main/java/fr/univtln/mapare/dao/DAO.java
+++ Server/src/main/java/fr/univtln/mapare/dao/DAO.java
@@ -1,6 +1,8 @@
 package fr.univtln.mapare.dao;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 
 import java.util.List;
@@ -10,11 +12,17 @@ public abstract class DAO<E> {
     protected EntityManager entityManager;
 
     public void persist(E entity) {
+        EntityTransaction transaction =  entityManager.getTransaction();
+        transaction.begin();
         entityManager.persist(entity);
+        transaction.commit();
     }
 
     public void update(E entity) {
+        EntityTransaction transaction =  entityManager.getTransaction();
+        transaction.begin();
         entityManager.merge(entity);
+        transaction.commit();
     }
 
     public void remove(E entity) {
