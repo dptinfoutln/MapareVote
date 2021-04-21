2b1b33847849be56d828b1560843f397d4500e4a
Nico <nico.marguerit@gmail.com>
2021-04-21 09:19:06 +0200

    adjusts and adds exceptions

--- Server/src/main/java/fr/univtln/mapare/exceptions/IllegalArgumentException.java
+++
@@ -1,9 +0,0 @@
-package fr.univtln.mapare.exceptions;
-
-import jakarta.ws.rs.core.Response;
-
-public class IllegalArgumentException extends BusinessException {
-    public IllegalArgumentException() {
-        super(Response.Status.NOT_ACCEPTABLE);
-    }
-}
