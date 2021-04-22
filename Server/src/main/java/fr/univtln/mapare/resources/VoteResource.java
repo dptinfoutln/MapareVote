c45da356591ca525f11c47f27767cc9cf2215903
François Palma <francois-palma@etud.univ-tln.fr>
2021-04-22 10:02:33 +0200

    some bugfixing

--- Server/src/main/java/fr/univtln/mapare/resources/VoteResource.java
+++ Server/src/main/java/fr/univtln/mapare/resources/VoteResource.java
@@ -101,6 +101,9 @@ public class VoteResource {
         if (vote.getEndDate() == null && !vote.isIntermediaryResult())
             throw new ForbiddenException("Vote with no end date and no intermediary results: invalid.");
         vote.setId(0);
+        if (vote.getAlgo() == "borda"){
+            vote.setMaxChoices(vote.getChoices().size());
+        }
         for (Choice c : vote.getChoices())
             c.setVote(vote);
         try {
