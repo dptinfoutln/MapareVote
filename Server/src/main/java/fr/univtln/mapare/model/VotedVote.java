90392189be4c5d7e5b5f4d354da6eb82aa291d3b
El-Sashok <reaubourg.alex@gmail.com>
2021-04-23 12:26:20 +0200

    fixed VotedVote token buffer bug

--- Server/src/main/java/fr/univtln/mapare/model/VotedVote.java
+++ Server/src/main/java/fr/univtln/mapare/model/VotedVote.java
@@ -43,10 +43,13 @@ public class VotedVote implements Serializable {
     public VotedVote(Vote vote, User user) {
         this.vote = vote;
         this.user = user;
-            Base64.Encoder base64Encoder = Base64.getUrlEncoder();
-            this.token = base64Encoder.encodeToString(ArrayUtils.addAll(
-                    SerializationUtils.serialize(user),
-                    SerializationUtils.serialize(vote)));
+
+        SecureRandom secureRandom = new SecureRandom();
+        Base64.Encoder base64Encoder = Base64.getUrlEncoder();
+
+        byte[] randomBytes = new byte[24];
+        secureRandom.nextBytes(randomBytes);
+        this.token = base64Encoder.encodeToString(randomBytes);
     }
 
 }
