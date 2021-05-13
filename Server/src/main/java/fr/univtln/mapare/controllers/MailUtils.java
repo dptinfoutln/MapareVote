c24293f5251ed57675110fbf5378d0c4ee3fcf41
onilateur <francois-palma@etud.univ-tln.fr>
2021-05-13 14:30:46 +0200

    Small changes, also records order in which people lose in STV

--- Server/src/main/java/fr/univtln/mapare/controllers/MailUtils.java
+++ Server/src/main/java/fr/univtln/mapare/controllers/MailUtils.java
@@ -17,7 +17,8 @@ public abstract class MailUtils {
     private MailUtils() {}
 
     public static void sendConfirmationMail(User user) {
-        sendMail(user, "Bonjour " + user.getFirstname() + " " + user.getLastname() + "!\n\nVous vous êtes " +
+        sendMail("[MapareVote] Code de confirmation!", user,
+                "Bonjour " + user.getFirstname() + " " + user.getLastname() + "!\n\nVous vous êtes " +
                 "inscrit à MapareVote dans le but de faire connaître votre opinion et d'interroger celle " +
                 "des autres sur des sujets divers et variés.\n\n\n" +
                 "Pour valider votre compte il suffit de cliquer sur le lien suivant: " + AUTH_LINK + user.getId() +
@@ -25,13 +26,14 @@ public abstract class MailUtils {
     }
 
     public static void sendInvitationMail(Vote vote, User invitee) {
-        sendMail(invitee, "Bonjour " + invitee.getFirstname() + " " + invitee.getLastname() + "!\n\n" +
+        sendMail("[MapareVote] Invitation à un vote!", invitee,
+                "Bonjour " + invitee.getFirstname() + " " + invitee.getLastname() + "!\n\n" +
                 "Vous avez été invité par " + vote.getVotemaker().getFirstname() + " " +
                 vote.getVotemaker().getLastname() + " à participer a un vote privé intitulé: \"" + vote.getLabel() +
-                "\"!\n\nPour participer il vous suffit de cliquer sur ce lien: " + VOTING_LINK + vote.getId() + ".");
+                "\" !\n\nPour participer il vous suffit de cliquer sur ce lien: " + VOTING_LINK + vote.getId() + ".");
     }
 
-    public static void sendMail(User user, String content) {
+    public static void sendMail(String title, User user, String content) {
         int port = 465;
         Properties properties = new Properties();
         properties.put("mail.smtp.user", SERVER_EMAIL_ADDRESS);
@@ -56,7 +58,7 @@ public abstract class MailUtils {
 
         MimeMessage msg = new MimeMessage(session);
         try {
-            msg.setSubject("Code de confirmation");
+            msg.setSubject(title);
             msg.setFrom(new InternetAddress(SERVER_EMAIL_ADDRESS));
             msg.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
             msg.setText(content);
